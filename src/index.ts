f03bdc2756fe9af7781adfb88fc96b82dcc7141e
Bruno Alencar <alencarb98@gmail.com>
2025-04-07 15:03:23 -0300

    ref: adicionado urls, middleware e novas rotas

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,43 @@ import express from "express"
 import path from "path"
 import { env } from "./config/env"
 import movieRoutes from "./routes/movieRoutes";
+import expressLayouts from "express-ejs-layouts"
 
 const app = express();
+const urls = {
+    homeUrl: "/",
+    moviesUrl: "/movies",
+    seriesUrl: "/series",
+}
 
-app.use(express.static(path.join(__dirname, "../public")))
+// Middleware global para passar urls em todas as views
+app.use((req, res, next) => {
+    res.locals.urls = urls;
+    next();
+});
 
+// view engine e layouts
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+app.set("layout", "./layout");
+
+// Static files
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Routes
 app.use('/api/movies', movieRoutes);
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.render("pages/index");
+})
+
+app.get("/movies", (req, res) => {
+    res.render("pages/movies");
+})
+
+app.get("/series", (req, res) => {
+    res.render("pages/tv_show");
 })
 
 app.listen(env.PORT, () => {
