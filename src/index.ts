47a249b0684950c73a7987c6f9aaef2fa3628bb3
Bruno Alencar <alencarb98@gmail.com>
2025-04-07 08:08:39 -0300

    feat: index file with home route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from "express"
+import path from "path"
+import { env } from "./config/env"
+import movieRoutes from "./routes/movieRoutes";
 
-const app = express()
+const app = express();
+
+app.use('/api/movies', movieRoutes);
 
 app.get("/", (req, res) => {
-    res.send("working");
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
-app.listen(3000, () => {
+app.listen(env.PORT, () => {
     console.log("app working");
 })
+
+export default app;
