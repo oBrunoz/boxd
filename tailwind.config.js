c10944c5a691e29b82f7d1c236acc70ae5598b5f
Bruno Alencar <alencarb98@gmail.com>
2025-04-07 08:11:01 -0300

    docs: packages

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./public", "./src/**/*.{ts,tsx,js,jsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
