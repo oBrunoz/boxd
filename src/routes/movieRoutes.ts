53dd1a56b291640af6254940de11217ff981c87b
Bruno Alencar <alencarb98@gmail.com>
2025-04-08 18:08:41 -0300

    ref: adicionado multi ao router

--- src/routes/movieRoutes.ts
+++ src/routes/movieRoutes.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import { getPopularMovies, getMovieDetails, getMovieVideos, getGenres, searchMovies } from '../controllers/movieController';
+import { getPopularMovies, getMovieDetails, getMovieVideos, getGenres, searchMulti } from '../controllers/movieController';
 
 const router = express.Router();
 
 router.get("/popular", getPopularMovies);
-// router.get("/search", searchMovies);
-router.get("/:id", getMovieDetails);
-router.get("/:id/videos", getMovieVideos);
+router.get("/multi", searchMulti);
 router.get("/genres", getGenres);
+router.get("/:id/videos", getMovieVideos);
+router.get("/:id", getMovieDetails);
 
 export default router;
